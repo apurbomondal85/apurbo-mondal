ad20d544a96ed54964a368fddcc2577f40714fb7
Apurbo-Mondal <apurbo.simt.2020@gmail.com>
2023-01-16 23:42:35 +0600

    hero section is complete

---
+++ main.js
@@ -0,0 +1,9 @@
+
+var typed = new Typed('.typed', {
+    // Waits 1000ms after typing "First"
+    strings: ['web developer.', 'front-End developer.'],
+    typeSpeed: 50,
+    loop: true,
+    backDelay: 900,
+    backSpeed: 30,
+  });
