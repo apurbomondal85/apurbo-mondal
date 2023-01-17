9e83c73fd8515525264c8018fd2f11d7abe281ce
Apurbo-Mondal <apurbo.simt.2020@gmail.com>
2023-01-17 20:25:19 +0600

    my responsive portfolio website is complete

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+// text-typed-auto 
 var typed = new Typed('.typed', {
     // Waits 1000ms after typing "First"
     strings: ['web developer.', 'front-End developer.'],
@@ -7,3 +7,29 @@ var typed = new Typed('.typed', {
     backDelay: 900,
     backSpeed: 30,
   });
+
+  // scroll-top
+
+  const home = document.querySelector("#home");
+
+  const footerElem = document.querySelector(".footer-section");
+
+  const scrollElement = document.createElement('div');
+
+scrollElement.classList.add("scrollTop-style");
+
+scrollElement.innerHTML = `<span class="scroll-top"><i class="fa-solid fa-arrow-up"></i></span>`;
+
+footerElem.after(scrollElement);
+
+const scrollTop = () =>{
+  home.scrollIntoView({behavior: "smooth"});
+};
+
+scrollElement.addEventListener('click',scrollTop);
+
+
+// animation aos 
+  AOS.init(
+  );
+
